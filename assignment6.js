9b9483bc92414e48b7c22ef83342a56a75083772
subodhghimire99 <subodhghimire99@gmail.com>
2019-10-03 21:03:19 +0530

    Assignment 6 with second part added

--- assignment6.js
+++ assignment6.js
@@ -4,10 +4,9 @@ john = {
     bill : [124, 48, 268, 180, 42],
     tips : [],
     calculateTip(){
-        billarr = this.bill;
         var tip;
-        var totalTip;
         for(var i=0; i<this.bill.length; i++){
+            var totalTip;
             switch(true){
                 case (this.bill[i] > 0 && this.bill[i] < 50) :
                     tip = this.bill[i] * 0.2;
@@ -22,10 +21,52 @@ john = {
                     tip = 0;
             }
         this.tips.push(tip);
-        totalTip = totalTip + tip;
         }
-        this.totalTip = totalTip;
     },
 }
 john.calculateTip();
+var totalWithTip = [];
+for(i=0; i<john.tips.length; i++){
+    let withTip = john.tips[i] + john.bill[i];
+    totalWithTip.push(withTip);
+}
+john.totalWithTip = totalWithTip;
+console.log("The final john object is : ");
 console.log(john);
+console.log(`\nBills amount are : ${john.bill}\n\nTips are : ${john.tips}\n\nAmount with tips added are : ${john.totalWithTip}\n\n`);
+
+// Assignment part 2
+console.log("Mark object : part II results \n")
+mark = {
+    bill : [77, 375, 110, 45],
+    tip : [],
+}
+calculateTip = function(){
+    var tip;   
+    for(var i=0; i<mark.bill.length; i++){
+        switch(true){
+            case (mark.bill[i] > 0 && mark.bill[i] < 100) :
+                tip = mark.bill[i] * 0.2;
+                break;
+            case (mark.bill[i] > 100 && mark.bill[i] < 300) :
+                tip = mark.bill[i] * 0.15;
+                break;
+            case (mark.bill[i] > 300) :
+                tip = mark.bill[i] * 0.1;
+                break;
+            default:
+                tip = 0;
+        }
+        mark.tip.push(tip);
+    }
+}
+var avgTip = function(){
+    var sum = 0;
+    for(i=0; i<mark.tip.length; i++){
+        sum = sum + mark.tip[i];
+    }
+    return sum/mark.tip.length;
+}
+calculateTip();
+console.log("The tips array is : " + mark.tip)
+console.log("The average tip of mark is : " + avgTip());
